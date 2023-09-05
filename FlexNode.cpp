3dd5b1ebef269c6ff98e45ede9a240807a4b80f9
MoggoCodes <amoghagarwal@ufl.edu>
2023-09-05 14:48:23 -0400

    init

---
+++ FlexNode.cpp
@@ -0,0 +1,102 @@
+#include "FlexNode.h"
+
+bool FlexNode::setNode(Type newType) {
+    if(containsData) {return false;}
+    containsData = true;
+    type = newType;
+    return true;
+}
+
+bool FlexNode::resetNode() {
+    if(!containsData) {return false;}
+    
+    switch(type) {
+        case Type::INT: delete num; break;
+        case Type::DOUBLE: delete dbl; break;
+        case Type::CHAR: delete ch; break;
+        case Type::STRING: delete[] ch; break;
+        case Type::BOOL: delete boo; break;
+        default: return false;                
+    }
+    
+    type = Type::UNINITIALIZED;
+    containsData = false;
+    return true;
+}
+
+bool FlexNode::addInt(int intToAdd) {
+    if(!setNode(Type::INT)) {return false;}
+    num = new int(intToAdd);
+    return true;
+}
+
+bool FlexNode::addDouble(double doubleToAdd) {
+    if(!setNode(Type::DOUBLE)) {return false;}
+    dbl = new double(doubleToAdd);
+    return true;
+}
+
+bool FlexNode::addChar(char charToAdd) {
+    if(!setNode(Type::CHAR)) {return false;}
+    ch = new char(charToAdd);
+    return true;
+}
+
+bool FlexNode::addString(std::string strToAdd) {
+    if(!setNode(Type::STRING)) {return false;}
+    ch = new char[strToAdd.length() + 1];
+    copy(strToAdd.begin(), strToAdd.end(), ch);
+    ch[strToAdd.length()] = '\0';
+    return true;
+}
+
+bool FlexNode::addBool(bool boolToAdd) {
+    if(!setNode(Type::BOOL)) {return false;}
+    boo = new bool(boolToAdd);
+    return true;
+}
+
+bool FlexNode::changeInt(int newInt) {
+    if(type != Type::INT) {return false;}
+    if(!resetNode()) {return false;}
+    return addInt(newInt);
+}
+
+bool FlexNode::changeDouble(double newDbl) {
+    if(type != Type::DOUBLE) {return false;}
+    if(!resetNode()) {return false;}
+    return addDouble(newDbl);
+}
+
+bool FlexNode::changeChar(char newChar) {
+    if(type != Type::CHAR) {return false;}
+    if(!resetNode()) {return false;}
+    return addChar(newChar);
+}
+
+bool FlexNode::changeString(std::string newStr) {
+    if(type != Type::STRING) {return false;}
+    if(!resetNode()) {return false;}
+    return addString(newStr);
+}
+
+bool FlexNode::changeBool(bool newBool) {
+    if(type != Type::BOOL) {return false;}
+    if(!resetNode()) {return false;}
+    return addBool(newBool);
+}
+
+std::ostream &FlexNode::print(std::ostream &out) {
+    switch(type) {
+        case Type::INT: return out << *num; break;
+        case Type::DOUBLE: return out << *dbl; break;
+        case Type::CHAR: return out << *ch; break;
+        case Type::STRING: return out << *ch; break;
+        case Type::BOOL: return out << *boo; break;
+        default: return out;
+    }
+}
+
+std::ostream& operator<<(std::ostream& out, FlexNode flexNode) {
+    return flexNode.print(out);
+}
--- FlexNode.h
+++ FlexNode.h
@@ -7,112 +7,35 @@ enum class Type {UNINITIALIZED, INT, DOUBLE, CHAR, STRING, BOOL};
 
 class FlexNode {
 private:
+    bool containsData = false;
+    Type type = Type::UNINITIALIZED;
+
     int* num = nullptr;
     double* dbl = nullptr;
     char* ch = nullptr;
     bool* boo = nullptr;
 
-    bool containsData = false;
-    Type type = Type::UNINITIALIZED;
-
-    bool addData(Type newType) {
-        if(containsData) {return false;}
-        containsData = true;
-        type = newType;
-        return true;
-    }
-
-    bool removeData() {
-        if(!containsData) {return false;}
-        switch(type) {
-            case Type::INT: delete num; break;
-            case Type::DOUBLE: delete dbl; break;
-            case Type::CHAR: delete ch; break;
-            case Type::STRING: delete[] ch; break;
-            case Type::BOOL: delete boo; break;
-            default: return false;                
-        }
-        type = Type::UNINITIALIZED;
-        containsData = false;
-        return true;
-    }
+    bool setNode(Type newType);
+    bool resetNode();
 
 public:
     FlexNode() {}
-    bool isEmpty() {return !containsData;}
-    bool addInt(int intToAdd) {
-        if(!addData(Type::INT)) {return false;}
-        num = new int(intToAdd);
-        return true;
-    }
-    bool addDouble(double doubleToAdd) {
-        if(!addData(Type::DOUBLE)) {return false;}
-        dbl = new double(doubleToAdd);
-        return true;
-    }
-    bool addChar(char charToAdd) {
-        if(!addData(Type::CHAR)) {return false;}
-        ch = new char(charToAdd);
-        return true;
-    }
-    bool addString(std::string strToAdd) {
-        if(!addData(Type::STRING)) {return false;}
-        ch = new char[strToAdd.length() + 1];
-        copy(strToAdd.begin(), strToAdd.end(), ch);
-        ch[strToAdd.length()] = '\0';
-        return true;
-    }
-    bool addBool(bool boolToAdd) {
-        if(!addData(Type::BOOL)) {return false;}
-        boo = new bool(boolToAdd);
-        return true;
-    }
-    bool clear() {return removeData();}
-
-    bool changeInt(int newInt) {
-        if(type != Type::INT) {return false;}
-        if(!removeData()) {return false;}
-        return addInt(newInt);
-    }
-
-    bool changeDouble(double newDbl) {
-        if(type != Type::DOUBLE) {return false;}
-        if(!removeData()) {return false;}
-        return addDouble(newDbl);
-    }
-
-    bool changeChar(char newChar) {
-        if(type != Type::CHAR) {return false;}
-        if(!removeData()) {return false;}
-        return addChar(newChar);
-    }
-
-    bool changeString(std::string newStr) {
-        if(type != Type::STRING) {return false;}
-        if(!removeData()) {return false;}
-        return addString(newStr);
-    }
     
-    bool changeBool(bool newBool) {
-        if(type != Type::BOOL) {return false;}
-        if(!removeData()) {return false;}
-        return addBool(newBool);
-    }
-
+    bool isEmpty() {return !containsData;}
+    bool clear() {return resetNode();}
     Type getType() {return type;}
+    
+    bool addInt(int intToAdd);
+    bool addDouble(double doubleToAdd);
+    bool addChar(char charToAdd);
+    bool addString(std::string strToAdd);
+    bool addBool(bool boolToAdd);
 
-    std::ostream& print(std::ostream& out) {
-        switch(type) {
-            case Type::INT: return out << *num; break;
-            case Type::DOUBLE: return out << *dbl; break;
-            case Type::CHAR: return out << *ch; break;
-            case Type::STRING: return out << *ch; break;
-            case Type::BOOL: return out << *boo; break;
-            default: return out;
-        }
-    }
+    bool changeInt(int newInt);
+    bool changeDouble(double newDbl);
+    bool changeChar(char newChar);
+    bool changeString(std::string newStr);
+    bool changeBool(bool newBool);
+
+    std::ostream& print(std::ostream& out);
 };
-
-std::ostream& operator<<(std::ostream& out, FlexNode flexNode) {
-    return flexNode.print(out);
-}
